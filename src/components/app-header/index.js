6608e56d54e21897d298247d8489a37da6dd488f
dindincow <dindincow@gmail.com>
2021-09-19 21:47:24 +0800

    header 佈局

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,9 +1,19 @@
 import React,{memo} from 'react'
-
+import {HeaderWrapper,HeaderLeft,HeaderRight} from './style'
 export default memo(function HYAppHeader() {
     return (
-        <div>
-            HYAppHeader
-        </div>
+        <HeaderWrapper>
+            <div className="content wrap-v1">
+                <HeaderLeft>
+                    <a href="#/" className="logo sprite_01">网易云音乐</a>
+                </HeaderLeft>
+                <HeaderRight>
+                    {/* <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />}/>
+                    <div className="center">创作者中心</div>
+                    <div>登录</div> */}
+                </HeaderRight>
+            </div>
+            <div className="divider"></div>
+        </HeaderWrapper>
     )
 })
